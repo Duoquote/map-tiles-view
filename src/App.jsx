cf7b04b98e7f84f776fe8c5583a898bc7ce42754
duoquote <16341937+Duoquote@users.noreply.github.com>
2024-07-30 17:28:11 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect } from "react";
+import { Box } from "@mui/material";
+import L from "leaflet";
+import { MapContainer } from "react-leaflet/MapContainer"
+import { TileLayer } from "react-leaflet/TileLayer"
+import { useMap } from "react-leaflet/hooks"
+
+const Tiles = () => {
+  const map = useMap();
+
+  useEffect(() => {
+    const layer = new L.GridLayer();
+    layer.createTile = (coords) => {
+      const tile = L.DomUtil.create("div");
+      tile.style.fontSize = "16pt";
+      tile.style.fontWeight = "bold";
+      tile.style.border = "1px solid black";
+      tile.innerHTML = `${coords.z}, ${coords.x}, ${coords.y}`;
+      return tile;
+    };
+    layer.addTo(map);
+
+    return () => {
+      map.removeLayer(layer);
+    };
+  }, []);
+
+  return null;
+};
+
+export default function App() {
+  return (
+    <Box sx={{ height: "100%", width: "100%" }}>
+      <MapContainer
+        center={[41, 29]}
+        zoom={13}
+        style={{ height: "100%", width: "100%" }}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Tiles />
+      </MapContainer>
+    </Box>
+  );
+}
